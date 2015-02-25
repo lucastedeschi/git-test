df0aaa65c58402016fa99fc5176e0fbec20d7937
41406133 <41406133@090017-P33S13.macklabs.br>
2015-02-25 09:56:35 -0300

    Criação de CSS Mudança do nome Mudança de estilor do nome

--- index.php
+++ index.php
@@ -1,10 +1,20 @@
-<?php
+<!doctype html>
+<html>
+    <head>
+        <title>Teste</title>
+        <link rel="stylesheet" type="text/css" href="style.css">
+        <meta charset="UTF-8"/>
+    </head>
+    <body>
+        <?php
 
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+        /*
+         * To change this template, choose Tools | Templates
+         * and open the template in the editor.
+         */
 
-  echo 12314;
-  echo 'vinicius';
-?>
+          echo 12314;
+          echo '<h1 class="name">lucas</h1>';
+        ?>
+    </body>
+</html>
