e550f65f479d57c37425547d808a57c7e0647a5b
41406133 <41406133@090017-P33S13.macklabs.br>
2015-02-26 11:58:39 -0300

    Aula 42, feito o 1

--- aula42_ex.php
+++ aula42_ex.php
@@ -3,13 +3,27 @@
     <head>
         <meta charset="UTF-8">
         <title>Aula 4.2</title>
+        <link rel="stylesheet" type="text/css" href="style.css">
     </head>
     <body>
+        <h1>Exercício 1 - Primos</h1>
+            <?php
+                echo"<table><tr><th>ID</th><th>NOME</th><th>DESC</th></tr>";
+                for($i=0;$i<7;$i++){
+                    if($i%2 == 0){
+                        echo"<tr><td>id</td><td>nome</td><td>desc</td></tr>";
+                    }
+                    else{
+                        echo"<tr class='tab'><td>id</td><td>nome</td><td>desc</td></tr>";
+                    }
+                }
+                echo"</table>";
+            ?>
+        <hr><br>
         <h1>Exercício 2 - Primos</h1>
             <?php 
                 $x = 113; 
                 $verifica = true;
-
                     if ($x == 1){
                         echo"$x não é primo";
                     }
